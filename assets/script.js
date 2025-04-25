1ea3cb5485d7cb147631af90353ce1c2dba68b36
bhatito <bhatitoputra86@gmail.com>
2025-04-25 19:20:12 +0700

    first

---
+++ assets/script.js
@@ -0,0 +1,56 @@
+window.addEventListener('scroll', ()=>{
+    document.querySelector('nav').classList.toggle('scrolling', scrollY > 50);
+})
+
+
+var portofolioTabs = document.getElementsByClassName("portofolio-tab");
+var tabContents = document.getElementsByClassName("tab-content");
+
+function tabOpen(x){
+    for(portofolioTab of portofolioTabs){
+        portofolioTab.classList.remove("active");
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active-content");
+    }
+    event.currentTarget.classList.add("active");
+    document.getElementById(x).classList.add("active-content");
+}
+
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = function(){
+    themeBtn.classList.toggle('ri-sun-line');
+    if(themeBtn.classList.contains('ri-sun-line')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
+
+
+//RESPONSIVE
+const navList = document.querySelector('.navlist');
+const menuBtn = document.querySelector('.ri-menu-line');
+
+menuBtn.onclick = function(){
+    navList.classList.toggle('active');
+    menuBtn.classList.toggle('ri-arrow-up-double-line');
+}
+
+const sr = ScrollReveal({
+    distance: '80px',
+    duration: 2500,
+    delay: 200,
+    reset: true,
+})
+
+sr.reveal('.home-container .left', {origin: 'top'});
+sr.reveal('.home-container .right', {origin: 'left'});
+sr.reveal('.about-container .right', {origin: 'top'});
+sr.reveal('.about-container .left', {origin: 'left'});
+sr.reveal('.services-container', {origin: 'bottom'});
+sr.reveal('.portofolio-container', {origin: 'left'});
+sr.reveal('.portofolio-buttons', {origin: 'top'});
+sr.reveal('.contact-container', {origin: 'top'});
